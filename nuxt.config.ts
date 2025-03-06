92b871d6f4fcd82e0ccf7b461dcb9a4070786b24
NRaykov <nediotenev@gmail.com>
2025-03-06 19:37:39 +0200

    Create pages directory. Create events page. Create event detailed page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,18 @@
-import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+  alias: {
+    "@": resolve(__dirname, '/'),
   },
+
+  css: ['~/assets/scss/main.scss'],
+
+  postcss: {
+    plugins: { 
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  },
+
+  compatibilityDate: "2025-03-06",
 });
